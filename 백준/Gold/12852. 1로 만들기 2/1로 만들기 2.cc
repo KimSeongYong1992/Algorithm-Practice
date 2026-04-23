05932ccc73053fb5307f3fcc6974cc89ab679cc5
Kimseongyong <147125447+KimSeongYong1992@users.noreply.github.com>
2026-04-23 14:50:38 +0900

    [Gold V] Title: 1로 만들기 2, Time: 8 ms, Memory: 9836 KB -BaekjoonHub

--- 백준/Gold/12852. 1로 만들기 2/1로 만들기 2.cc
+++ 백준/Gold/12852. 1로 만들기 2/1로 만들기 2.cc
@@ -1,3 +1,10 @@
+/******************************************************************************
+
+                              Online C++ Compiler.
+               Code, Compile, Run and Debug C++ program online.
+Write your code in this editor and press "Run" button to compile and execute it.
+
+*******************************************************************************/
 
 #include <iostream>
 #include <vector>
@@ -47,10 +54,6 @@ void PrintAnswer(int Num)
 
 int main()
 {
-    ios_base::sync_with_stdio(false);
-	cin.tie(nullptr);
-	cout.tie(nullptr);
-    
     int Number = 0;
     cin >> Number;
     
